e4a15a9e923d2d9f74c055ece5642ed0d3330053
V_folder <f4789343@gmail.com>
2026-09-07 00:02:12 +0530

    Added visualization

--- visualization.py
+++ visualization.py
@@ -1,1 +1,18 @@
-from 
+import matplotlib.pyplot as plt
+from scikit_learn import cm, y_test
+import numpy as np
+
+classes = np.unique(y_test)
+plt.imshow(cm)
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix - Naive bayes')
+plt.xticks(range(len(classes)), classes)
+plt.yticks(range(len(classes)), classes)
+
+for i in range(len(classes)):
+  for j in range(len(classes)):
+    plt.text(j, i, cm[i][j], ha='center',va='center')
+plt.colorbar()
+plt.savefig('Confusion_Matrix.png')
+plt.show()
